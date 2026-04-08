264d1976dfef2c96b8f7cd92f47a9cb41b361ca8
iVAN <88724353+ivan-hc@users.noreply.github.com>
2026-04-09 00:55:03 +0200

    Add stable, edge and candidate

--- spotify-snap2appimage.sh
+++ spotify-snap2appimage.sh
@@ -8,34 +8,62 @@ cd ./tmp || exit 1
 
 # DOWNLOAD APPIMAGETOOL
 if ! test -f ./appimagetool; then
-	wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool || exit 1
+	wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
 	chmod a+x ./appimagetool
 fi
 
-# DOWNLOAD THE SNAP PACKAGE
-if ! test -f ./*.snap; then
-	wget -q "$(curl -H 'Snap-Device-Series: 16' http://api.snapcraft.io/v2/snaps/info/spotify --silent | sed 's/\[{/\n/g; s/},{/\n/g' | grep -i "stable" | head -1 | sed 's/[()",{} ]/\n/g' | grep "^http")" || exit 1
-fi
+# CREATE SPOTIFY APPIMAGES
 
-# EXTRACT THE SNAP PACKAGE AND CREATE THE APPIMAGE
-unsquashfs -f ./*.snap
-mkdir -p "$APP".AppDir
-VERSION=$(cat ./squashfs-root/meta/*.yaml | grep "^version" | head -1 | cut -c 10-)
-mv ./squashfs-root/usr ./"$APP".AppDir/
-cp -r ./"$APP".AppDir/usr/share/spotify/icons/*128* ./"$APP".AppDir/"$APP".png
-cp -r ./"$APP".AppDir/usr/share/spotify/*.desktop ./"$APP".AppDir/
-sed -i "s#Icon=.*#Icon=$APP#g" ./"$APP".AppDir/*.desktop
+_create_spotify_appimage() {
+	# DOWNLOAD THE SNAP PACKAGE
+	if ! test -f ./*.snap; then
+		if wget --version | head -1 | grep -q ' 1.'; then
+			wget -q --no-verbose --show-progress --progress=bar "$(curl -H 'Snap-Device-Series: 16' http://api.snapcraft.io/v2/snaps/info/spotify --silent | sed 's/\[{/\n/g; s/},{/\n/g' | grep -i "$CHANNEL" | head -1 | sed 's/[()",{} ]/\n/g' | grep -Eoi "^http.*snap$")"
+		else
+			wget "$(curl -H 'Snap-Device-Series: 16' http://api.snapcraft.io/v2/snaps/info/spotify --silent | sed 's/\[{/\n/g; s/},{/\n/g' | grep -i "$CHANNEL" | head -1 | sed 's/[()",{} ]/\n/g' | grep -Eoi "^http.*snap$")"
+		fi
+	fi
 
-cat >> ./"$APP".AppDir/AppRun << 'EOF'
-#!/bin/sh
-HERE="$(dirname "$(readlink -f "${0}")")"
-export UNION_PRELOAD="${HERE}"
-exec "${HERE}"/usr/bin/spotify "$@"
-EOF
-chmod a+x ./"$APP".AppDir/AppRun
+	# EXTRACT THE SNAP PACKAGE AND CREATE THE APPIMAGE
+	unsquashfs -f ./*.snap
+	mkdir -p "$APP".AppDir
+	VERSION=$(cat ./squashfs-root/meta/*.yaml | grep "^version" | head -1 | cut -c 10-)
 
-ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 \
-	-u "gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|Spotify-appimage|continuous|*x86_64.AppImage.zsync" \
-	./"$APP".AppDir Spotify-"$VERSION"-x86_64.AppImage || exit 1
+	mv ./squashfs-root/usr ./"$APP".AppDir/
+	cp -r ./"$APP".AppDir/usr/share/spotify/icons/*128* ./"$APP".AppDir/"$APP".png
+	cp -r ./"$APP".AppDir/usr/share/spotify/*.desktop ./"$APP".AppDir/
+	sed -i "s#Icon=.*#Icon=$APP#g" ./"$APP".AppDir/*.desktop
 
-cd .. && mv ./tmp/*AppImage* ./
+	cat <<-'HEREDOC' >> ./"$APP".AppDir/AppRun
+	#!/bin/sh
+	HERE="$(dirname "$(readlink -f "${0}")")"
+	export UNION_PRELOAD="${HERE}"
+	exec ${HERE}/spotify "$@"
+	HEREDOC
+	chmod a+x ./"$APP".AppDir/AppRun
+
+	ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 \
+	-u "gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|Spotify-appimage|continuous|*-$CHANNEL-*x86_64.AppImage.zsync" \
+	./"$APP".AppDir Spotify-"$CHANNEL"-"$VERSION"-x86_64.AppImage || exit 1
+}
+
+CHANNEL="stable"
+mkdir -p "$CHANNEL" && cp ./appimagetool ./"$CHANNEL"/appimagetool && cd "$CHANNEL" || exit 1
+_create_spotify_appimage
+cd ..
+mv ./"$CHANNEL"/*.AppImage* ./
+
+CHANNEL="candidate"
+mkdir -p "$CHANNEL" && cp ./appimagetool ./"$CHANNEL"/appimagetool && cd "$CHANNEL" || exit 1
+_create_spotify_appimage
+cd ..
+mv ./"$CHANNEL"/*.AppImage* ./
+
+CHANNEL="edge"
+mkdir -p "$CHANNEL" && cp ./appimagetool ./"$CHANNEL"/appimagetool && cd "$CHANNEL" || exit 1
+_create_spotify_appimage
+cd ..
+mv ./"$CHANNEL"/*.AppImage* ./
+
+cd ..
+mv ./tmp/*.AppImage* ./
